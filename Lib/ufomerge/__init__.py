526ab2984fa911e384c8d67e6ddffc06039c6359
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2023-07-18 11:47:49 +0100

    Also consider variation blocks

--- Lib/ufomerge/__init__.py
+++ Lib/ufomerge/__init__.py
@@ -503,7 +503,9 @@ class UFOMerger:
         # Collect all referenced lookups
         referenced = set()
         for feature in layout.statements:
-            if not isinstance(feature, (ast.FeatureBlock, ast.LookupBlock)):
+            if not isinstance(
+                feature, (ast.FeatureBlock, ast.LookupBlock, ast.VariationBlock)
+            ):
                 continue
             for statement in feature.statements:
                 if isinstance(statement, ast.LookupReferenceStatement):
